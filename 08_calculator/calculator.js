e4513753b478e2b4a461b1b2f02a09c6a2aab892
VirsatusCodes <donavynpence@yahoo.com>
2022-05-08 13:10:51 -0700

    exercise completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
+const add = function(a, b) {
+  return a + b;
 	
 };
 
-const subtract = function() {
+const subtract = function(a, b) {
+  return a - b;
 	
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let total = 0;
+	for (let i = 0; i < array.length; i++) {
+    total += array[i];
+  }
+  return total;
+};
+// couldve used array.reduce for the above and below function
+//same end but written faster for me if used. 
+const multiply = function(array) {
+  let total = array [0];
+	for (let i = 1; i < array.length; i++) {
+    total = array[i] * total;
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const power = function(a, b) {
+  let total = 1;
+	for (let i = 0; i < b; i++) {
+    total = total * a;
+  }
+  return total;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(a) {
+	let total = 1;
+  for(let i = a; i > 1 ;i-- ) {
+    total = i * total;
+  }
+  return total;
 };
 
 // Do not edit below this line
