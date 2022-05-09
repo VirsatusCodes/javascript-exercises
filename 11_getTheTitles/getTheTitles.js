6d73b2fdd210c141f61603d86cc033f2c97f1efa
VirsatusCodes <donavynpence@yahoo.com>
2022-05-08 21:34:55 -0700

    completed w/notes.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    return books.map(book => book.title)
+    };
 
-};
+//it was fairly easy identifying what tool to use, however for whatever reason
+//it was being extremely difficult in the implementation
+//i guess i had the syntax minorly wrong and everytime i "fixed" it
+//i messed something else up, oh well i got there eventually.
 
 // Do not edit below this line
 module.exports = getTheTitles;
